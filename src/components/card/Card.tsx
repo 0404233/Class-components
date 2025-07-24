9d0316a0f272a6ee660c0040fb424634de494b9f
0404233 <andrey0404233@gmail.com>
2025-07-24 13:28:33 +0300

    feat: replace Card class component to functional component

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,27 +1,24 @@
-import { Component } from 'react';
+import type { ReactElement } from 'react';
 import type { Description } from '../../types';
 import styles from './card.module.css';
 
 type Props = {
   name: string;
-  description?: Description;
+  description?: Description | null;
 };
 
-export default class Card extends Component<Props> {
-  render() {
-    const { name, description } = this.props;
-    return (
-      <div className={styles.card}>
-        <h3>{name}</h3>
-        {description && (
-          <>
-            <p>Base experience: {description.base_experience}</p>
-            <p>Height: {description.height}</p>
-            <p>Is default: {description.is_default ? 'Yes' : 'No'}</p>
-            <p>Weight: {description.weight}</p>
-          </>
-        )}
-      </div>
-    );
-  }
+export default function Card({ name, description }: Props): ReactElement {
+  return (
+    <div className={styles.card}>
+      <h3>{name}</h3>
+      {description && (
+        <>
+          <p>Base experience: {description.base_experience}</p>
+          <p>Height: {description.height}</p>
+          <p>Is default: {description.is_default ? 'Yes' : 'No'}</p>
+          <p>Weight: {description.weight}</p>
+        </>
+      )}
+    </div>
+  );
 }
