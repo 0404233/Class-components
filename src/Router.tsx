4847618ee1f963ce875d4f20327a5b0f18e82e75
0404233 <andrey0404233@gmail.com>
2025-07-25 12:07:37 +0300

    feat: add Error and About pages

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,10 +1,14 @@
 import { Route, Routes } from 'react-router-dom';
+import About from './pages/about/About';
+import Error from './pages/error/Error';
 import Main from './pages/main/Main';
 
 export default function Router() {
   return (
     <Routes>
       <Route path="/" element={<Main />} />
+      <Route path="/about" element={<About />} />
+      <Route path="*" element={<Error />} />
     </Routes>
   );
 }
