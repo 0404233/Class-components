1f5c945aff07dec8cea43e24a02fcef4597e18f5
0404233 <andrey0404233@gmail.com>
2025-07-30 18:24:57 +0300

    feat: implement selected items pane

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Router from './Router';
 import Header from './components/header/Header';
+import SelectedItems from './components/SelectItems';
 
 export default function App() {
   return (
     <>
       <Header />
       <Router />
+      <SelectedItems />
     </>
   );
 }
