70b327b57da4bb18eba6daf4ba410cb0342f883d
0404233 <andrey0404233@gmail.com>
2025-08-10 14:18:33 +0300

    feat: add queryClient params to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,26 +1,32 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import ErrorBoundary from './components/errorBoundary/ErrorBoundary.tsx';
 import './index.css';
 import App from './App.tsx';
 import { ThemeProvider } from './context/ThemeContext';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5,
+      retry: 2,
+    },
+  },
+});
 
 const rootElement = document.getElementById('root');
-const queryClient = new QueryClient();
 
-if (!rootElement) {
-  throw new Error('Root element not found');
-}
+if (!rootElement) throw new Error('Root element not found');
 
 createRoot(rootElement).render(
   <StrictMode>
-    <ErrorBoundary>
-      <ThemeProvider>
-        <QueryClientProvider client={queryClient}>
+    <QueryClientProvider client={queryClient}>
+      <ErrorBoundary>
+        <ThemeProvider>
           <App />
-        </QueryClientProvider>
-      </ThemeProvider>
-    </ErrorBoundary>
+        </ThemeProvider>
+      </ErrorBoundary>
+    </QueryClientProvider>
   </StrictMode>
 );
