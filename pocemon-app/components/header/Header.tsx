c67e6bb4cc025c0c520dda5a864152daf0f0467c
0404233 <andrey0404233@gmail.com>
2025-08-18 14:03:20 +0300

    App Router setup with i18n routing

--- pocemon-app/components/header/Header.tsx
+++ pocemon-app/components/header/Header.tsx
@@ -1,25 +1,25 @@
-import type { ReactNode, ReactElement } from 'react';
-import logo from './../../assets/logo.png';
-import styles from './header.module.css';
-import { Link } from 'react-router-dom';
-import ThemeSelector from '../themeSelector/ThemeSelector';
+// import type { ReactNode, ReactElement } from 'react';
+// import logo from './../../assets/logo.png';
+// import styles from './header.module.css';
+// import { Link } from 'react-router-dom';
+// import ThemeSelector from '../themeSelector/ThemeSelector';
 
-type Props = {
-  children?: ReactNode;
-};
+// type Props = {
+//   children?: ReactNode;
+// };
 
-export default function Header({ children }: Props): ReactElement {
-  return (
-    <header className={styles.header}>
-      <nav className={styles.nav}>
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-      </nav>
-      {children}
-      <div className={styles.logo}>
-        <img src={logo} alt="Logo" className={styles.logoImage} />
-      </div>
-      <ThemeSelector />
-    </header>
-  );
-}
+// export default function Header({ children }: Props): ReactElement {
+//   return (
+//     <header className={styles.header}>
+//       <nav className={styles.nav}>
+//         <Link to="/">Home</Link>
+//         <Link to="/about">About</Link>
+//       </nav>
+//       {children}
+//       <div className={styles.logo}>
+//         {/* <img src={logo} alt="Logo" className={styles.logoImage} /> */}
+//       </div>
+//       <ThemeSelector />
+//     </header>
+//   );
+// }
